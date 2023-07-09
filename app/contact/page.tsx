e1931f66c90b3ccb6e3f12b739286eebd9fe0355
jccdev45 <jccdev45@gmail.com>
2023-07-09 03:38:31 -0400

    Formspree

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Contact } from "lucide-react";
 import { useForm } from "react-hook-form";
@@ -7,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -20,8 +20,17 @@ import { WindowContent } from "@/components/window/window-content";
 import { WindowSidebar } from "@/components/window/window-sidebar";
 import { Textarea } from "@/components/ui/textarea";
 import { RainbowSeparator } from "@/components/rainbow-separator";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function ContactPage() {
+  const { toast } = useToast();
+
+  const [status, setStatus] = useState({
+    submitted: false,
+    submitting: false,
+    info: { error: false, msg: null || "" },
+  });
+
   const form = useForm<ContactSchemaValues>({
     resolver: zodResolver(ContactSchema),
     defaultValues: {
@@ -31,8 +40,40 @@ export default function ContactPage() {
     },
   });
 
+  const handleServerResponse = (ok: boolean, msg: string) => {
+    if (ok) {
+      setStatus({
+        submitted: true,
+        submitting: false,
+        info: { error: false, msg: msg },
+      });
+      form.reset();
+    } else {
+      setStatus({ ...status, info: { error: true, msg: msg } });
+    }
+  };
+
   function onSubmit(values: ContactSchemaValues) {
-    console.log(values);
+    setStatus((prevStatus) => ({ ...prevStatus, submitting: true }));
+
+    fetch("https://formspree.io/f/myyqyjzd", {
+      method: "POST",
+      body: JSON.stringify(values),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => {
+        handleServerResponse(
+          true,
+          "Thank you, your message has been submitted."
+        );
+      })
+      .catch((error) => {
+        handleServerResponse(false, error.response.data.error);
+      });
+
+    // console.log(values);
   }
 
   return (
@@ -42,6 +83,12 @@ export default function ContactPage() {
       menu={menuItems.default}
       bottomBar
     >
+      {status.submitting && (
+        <div className="absolute inset-x-0 top-0 z-10 grid bottom-6 place-items-center bg-white/90">
+          <div className="w-32 h-32 m-auto border-4 border-solid rounded-full border-windows-blue border-t-transparent animate-spin" />
+        </div>
+      )}
+
       <div className="absolute inset-x-0 top-0 flex flex-col p-2 overflow-scroll bottom-6 lg:p-0 lg:flex-row gap-y-2 justify-evenly">
         <WindowSidebar className="flex flex-col items-center justify-start w-full lg:p-2 md:justify-start gap-y-2 lg:w-1/3 lg:justify-start lg:border-r lg:border-windows-dark lg:shadow-inner lg:shadow-windows-dark">
           {/* <div className="flex flex-col items-center justify-start"> */}
@@ -112,7 +159,17 @@ export default function ContactPage() {
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="w-1/3 ml-auto">
+              <Button
+                type="submit"
+                className="w-1/3 ml-auto"
+                disabled={status.submitting}
+                onClick={() => {
+                  status.submitted &&
+                    toast({
+                      description: "Your message has been sent.",
+                    });
+                }}
+              >
                 Submit
               </Button>
             </form>
